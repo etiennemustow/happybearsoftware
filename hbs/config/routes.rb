df7f09f8d95e9c67cdde835883d9d0e3fd21e785
Etienne Mustow <etiennemustow@Etiennes-MBP.home>
2018-02-07 13:13:05 +0000

    Listings#new set as root of domain

--- hbs/config/routes.rb
+++ hbs/config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
 
   resources :listings
 
-  root 'welcome#index'
+  root 'listings#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
